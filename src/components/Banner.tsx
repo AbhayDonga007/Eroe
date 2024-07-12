c753d93a876e8e3f1d2222e8f9e665afe79a0975
Abhay Donga <93820026+AbhayDonga007@users.noreply.github.com>
2024-07-12 16:54:08 +0530

    initial commit

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,7 +10,6 @@ import 'swiper/css/scrollbar';
 import Image from 'next/image';
 
 import b1 from "@/images/banner5.png"
-import b2 from "@/images/Piyush Donga-images-1.jpg"
 import b3 from "@/images/banner1.png"
 import b4 from "@/images/banner2.png"
 import b5 from "@/images/banner3.png"
@@ -49,13 +48,6 @@ const Banner = (props: Props) => {
         <Image
             className="object-cover"
             src={b3}
-            alt="Picture of the author"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-            className="object-cover"
-            src={b2}
             alt="Picture of the author"
         />
       </SwiperSlide>
